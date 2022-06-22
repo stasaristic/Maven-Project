8f31ed7d285b8ee968bcbbba1a1762e3efb737b1
Stasa Ristic <risticstasa@gmail.com>
2022-06-22 13:17:26 +0200

    Initial commit

---
+++ child-project-2/src/test/java/HelloWorldTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+    }
+}
